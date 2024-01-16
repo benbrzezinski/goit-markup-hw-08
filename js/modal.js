8c82786fdde3ca3079a5f966729dfa0e59de2c41
Beniamin Brzeziński <benek9311@wp.pl>
2024-01-16 11:45:03 +0100

    Added new functionalities to modal and minor changes

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,35 @@
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+const serviceModal = document.querySelector("[data-modal]");
+const openModalBtn = document.querySelector("[data-modal-open]");
+const closeModalBtn = document.querySelector("[data-modal-close]");
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+const closeModalByKey = e => {
+  if (e.code === "Escape") {
+    serviceModal.classList.add("is-hidden");
+    bodyScrollLock.enableBodyScroll(document.body);
+    document.removeEventListener("keydown", closeModalByKey);
   }
-})();
+};
+
+const openModal = () => {
+  serviceModal.classList.remove("is-hidden");
+  bodyScrollLock.disableBodyScroll(document.body);
+  document.addEventListener("keydown", closeModalByKey);
+};
+
+const closeModal = () => {
+  serviceModal.classList.add("is-hidden");
+  bodyScrollLock.enableBodyScroll(document.body);
+  document.removeEventListener("keydown", closeModalByKey);
+};
+
+const closeModalByBackdrop = e => {
+  if (e.currentTarget === e.target) {
+    serviceModal.classList.add("is-hidden");
+    bodyScrollLock.enableBodyScroll(document.body);
+    document.removeEventListener("keydown", closeModalByKey);
+  }
+};
+
+openModalBtn.addEventListener("click", openModal);
+closeModalBtn.addEventListener("click", closeModal);
+serviceModal.addEventListener("click", closeModalByBackdrop);
